3c1eee5523a564db2a88793b157b6c40319be862
Yannick Lapp <yannick.lapp@cn-consult.eu>
2019-04-03 15:40:47 +0200

    Add boxfit dependency
    * Added bower
    * Added bower install command to provision

--- VagrantProvision/installProjectDependencies.sh
+++ VagrantProvision/installProjectDependencies.sh
@@ -7,3 +7,6 @@ echo "Installing project dependencies"
 echo "Executing \"npm install\" ..."
 cd /home/vagrant/project
 npm install --unsafe-perm=true
+
+cd src/web/resources
+/home/vagrant/project/node_modules/bower/bin/bower install --allow-root
